79a49e4131eee8b22fe6d3e45d217d65f09864f6
JfayTAFE <jimmydb92@gmail.com>
2024-06-10 19:52:21 +1000

    Merged WelcomeSButtons into WelcomeScreen.js

--- StaffDirectory/components/WelcomeScreen.js
+++ StaffDirectory/components/WelcomeScreen.js
@@ -1,10 +1,20 @@
+// WelcomeScreen.js
+
 import React from 'react';
-import { View, StyleSheet, Image, Dimensions } from 'react-native';
-import CustomButton from '../components/CustomButton';
+import { View, StyleSheet, Image, Dimensions, TouchableOpacity, Text } from 'react-native';
 
 const { width, height } = Dimensions.get('window');
+const buttonWidth = width * 0.8; // 80% of the screen width
 const logoWidth = width * 0.9; // 90% of the screen width
 const logoHeight = (logoWidth * 300) / 600; // Maintain aspect ratio of the logo
+
+const CustomButton = ({ title, onPress }) => {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
 
 const WelcomeScreen = ({ navigation }) => {
   return (
@@ -38,6 +48,20 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     width: '100%', // Ensure the button is centered horizontally
+  },
+  button: {
+    backgroundColor: '#ffffff', // Button background color
+    width: buttonWidth,
+    paddingVertical: 15, // Add space above/below text
+    borderRadius: 5, // Rounds the corners of the button
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10, // Adds space outside the button
+  },
+  buttonText: {
+    color: 'black', // Button text color
+    fontSize: 18,
+    fontFamily: 'Trebuchet MS', // Button font
   },
 });
 
